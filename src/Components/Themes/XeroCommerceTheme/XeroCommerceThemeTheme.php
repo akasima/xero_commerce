75ccd72b9627fe63d319c749990b7a49ded8523e
sirwoongke <fzwoongbin@naver.com>
2018-09-20 12:58:02 +0900

    xero_commerce_theme 추가

---
+++ src/Components/Themes/XeroCommerceTheme/XeroCommerceThemeTheme.php
@@ -0,0 +1,9 @@
+<?php
+namespace Xpressengine\Plugins\XeroCommerce\Components\Themes\XeroCommerceTheme;
+
+use Xpressengine\Theme\GenericTheme;
+
+class XeroCommerceThemeTheme extends GenericTheme
+{
+    protected static $path = 'xero_commerce/src/Components/Themes/XeroCommerceTheme';
+}
---
+++ src/Components/Themes/XeroCommerceTheme/info.php
@@ -0,0 +1,55 @@
+<?php
+return [
+    'view' => 'theme',
+    'setting' => [
+        'logo_title' => [
+            '_type' => 'text',
+            '_section' => '로고 설정',
+            'label' => '로고 타이틀',
+            'placeholder' => '로고 제목을 입력하세요',
+            'description' => '로고 제목을 입력하세요',
+        ],
+        'show_spot' => [
+            '_type' => 'select',
+            '_section' => '스팟 설정',
+            'label' => '스팟 출력 ',
+            'options' => [
+                'hide' => '출력 안 함',
+                'show' => '출력 함'
+            ]
+        ],
+        'spot_title' => [
+            '_type' => 'text',
+            '_section' => '스팟 설정',
+            'label' => '스팟 타이틀',
+            'placeholder' => '스팟에 출력할 제목을 입력하세요',
+            'description' => '스팟에 출력할 제목을 입력하세요',
+        ],
+        'spot_content' => [
+            '_type' => 'textarea',
+            '_section' => '스팟 설정',
+            'label' => '스팟 내용',
+            'placeholder' => '스팟에 출력할 내용을 입력하세요',
+            'description' => '스팟에 출력할 내용을 입력하세요',
+        ],
+        'spot_image' => [
+            '_type' => 'image',
+            '_section' => '스팟 설정',
+            'label' => '스팟 이미지',
+            'description' => '스팟에 출력할 이미지를 등록하세요'
+        ],
+        'gnb' => [
+            '_type' => 'menu',
+            '_section' => '메뉴 설정',
+            'label' => '메인메뉴',
+        ]
+    ],
+    'support' => [
+        'mobile' => true,
+        'desktop' => true
+    ],
+    'editable' => [
+        'theme.blade.php',
+        'gnb.blade.php'
+    ]
+];
