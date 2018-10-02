aa30fce78f51e2c4179d26732026adcefa666595
Hero Kim <kimyu711@xpressengine.com>
2018-10-02 10:59:25 +0900

    node_moudle 정리 (skin과 setting 구분)

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,89 +2,43 @@ var path = require('path');
 const VueLoader = require('vue-loader/lib/plugin')
 
 module.exports = [{
-    mode: 'development',
-    entry: '../src/Components/Skins/XeroCommerceDefault/src/app.js',
-    output: {
-        path: path.resolve(__dirname, '../src/Components/Skins/XeroCommerceDefault/assets/js'),
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            transform: '../src/Components/Skins/XeroCommerceDefault/src/transform.js'
-                        }
-                    },
-                    'css-loader',
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, './js'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          'css-loader',
         ]
-    },
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.js'
-        },
-        extensions: ['*', '.js', '.vue', '.json']
-    },
-    plugins: [
-        new VueLoader()
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
     ]
-},
-    {
-        mode: 'development',
-        entry: '../views/setting/app.js',
-        output: {
-            path: path.resolve(__dirname, './js'),
-            filename: 'index.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    loader: 'vue-loader'
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                transform: '../src/Components/Skins/XeroCommerceDefault/src/transform.js'
-                            }
-                        },
-                        'css-loader',
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
-                    use: [
-                        'file-loader'
-                    ]
-                }
-            ]
-        },
-        resolve: {
-            alias: {
-                'vue': 'vue/dist/vue.js'
-            },
-            extensions: ['*', '.js', '.vue', '.json']
-        },
-        plugins: [
-            new VueLoader()
-        ]
-    }
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+    new VueLoader()
+  ]
+}
 ]
---
+++ src/Components/Skins/XeroCommerceDefault/assets/src/app.js
@@ -0,0 +1,18 @@
+window.Vue = require('vue')
+window.BootstrapVue = require('bootstrap-vue');
+
+Vue.use(BootstrapVue);
+
+require('bootstrap/dist/css/bootstrap.css');
+require('bootstrap-vue/dist/bootstrap-vue.css');
+require('@fortawesome/fontawesome-free/js/all.min');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+
+Vue.component('test-component', require('./components/TestComponent').default);
+Vue.component('cart-component', require('./components/Cart/CartComponent').default);
+Vue.component('order-register-component', require('./components/Order/OrderRegisterComponent').default);
+Vue.component('dash-component', require('./components/DashComponent').default);
+
+var app = new Vue({
+  el: '#sub-container'
+});
