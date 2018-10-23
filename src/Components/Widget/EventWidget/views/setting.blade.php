6ff3801799ad8bffc4eccf27c42f86486393bb2c
sirwoongke <fzwoongbin@naver.com>
2018-10-23 18:56:15 +0900

    #2 EventWidget 초안 추가

---
+++ src/Components/Widget/EventWidget/views/setting.blade.php
@@ -0,0 +1,19 @@
+<div class="form-group">
+    <label>좌측 상품 ID</label>
+    <input class="form-control" name="left_product_id" value="{{ array_get($args, 'left_product_id') }}">
+</div>
+
+<div class="form-group">
+    <label>중앙 위 상품 ID</label>
+    <input class="form-control" name="center_up_product_id" value="{{ array_get($args, 'center_up_product_id') }}">
+</div>
+
+<div class="form-group">
+    <label>중앙 아래 상품 ID</label>
+    <input class="form-control" name="center_down_product_id" value="{{ array_get($args, 'center_down_product_id') }}">
+</div>
+
+<div class="form-group">
+    <label>오른쪽 상품 ID</label>
+    <input class="form-control" name="right_product_id" value="{{ array_get($args, 'right_product_id') }}">
+</div>
