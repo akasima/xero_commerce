67acea666282c5b55c475a3e83a45503aa15bc16
sirwoongke <fzwoongbin@naver.com>
2018-11-01 16:02:48 +0900

    #2 Event 위젯 스타일 적용

--- src/Components/Widget/EventWidget/Skins/Common/views/widget.blade.php
+++ src/Components/Widget/EventWidget/Skins/Common/views/widget.blade.php
@@ -1,14 +1,45 @@
-<div class="widget_event">
-    <table style="border: 1px solid #000000;">
-        <tr>
-            <td rowspan="2">{{ $products['left']['name'] }}</td>
-
-            <td>{{ $products['center_up']['name'] }}</td>
-            <td rowspan="2">{{ $products['center_down']['name'] }}</td>
-        </tr>
-
-        <tr>
-            <td>{{ $products['right']['name'] }}</td>
-        </tr>
-    </table>
+<div class="xe-shop">
+    <div class="container">
+        <h2 class="xe-shop-wiget-title">EVENT</h2>
+        <div class="cross-list">
+            <div class="large_img">
+                <a href="#">
+                    <img src="{{ $products['left']->getThumbnailSrc() }}">
+                    <div class="inner_box">
+                        <strong>{{ $products['left']->name }}</strong>
+                        <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
+                    </div>
+                </a>
+            </div>
+            <div class="small_box">
+                <div class="small_img">
+                    <a href="#">
+                        <img src="{{ $products['center_up']->getThumbnailSrc() }}">
+                        <div class="inner_box">
+                            <strong>{{ $products['center_up']->name }}</strong>
+                            <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="small_img">
+                    <a href="#">
+                        <img src="{{ $products['center_down']->getThumbnailSrc() }}">
+                        <div class="inner_box">
+                            <strong>{{ $products['center_down']->name }}</strong>
+                            <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="large_img">
+                <a href="#">
+                    <img src="{{ $products['right']->getThumbnailSrc() }}">
+                    <div class="inner_box">
+                        <strong>{{ $products['right']->name }}</strong>
+                        <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
