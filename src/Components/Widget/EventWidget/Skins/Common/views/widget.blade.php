fdcb7b1123a8d9e52f90bb6ce3dd447b1070e3f0
sirwoongke <fzwoongbin@naver.com>
2018-11-02 11:15:47 +0900

    #2 이미지 사이즈 지정

--- src/Components/Widget/EventWidget/Skins/Common/views/widget.blade.php
+++ src/Components/Widget/EventWidget/Skins/Common/views/widget.blade.php
@@ -6,7 +6,7 @@
         <div class="cross-list">
             <div class="large_img">
                 <a href="{{ route('xero_commerce::product.show', ['slug' => $products['left']->getSlug()]) }}">
-                    <img src="{{ $products['left']->getThumbnailSrc() }}">
+                    <img src="{{ $products['left']->getThumbnailSrc() }}" style="width: 367px; height: 490px;">
                     <div class="inner_box">
                         <strong>{{ $products['left']->name }}</strong>
                         <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
@@ -16,7 +16,7 @@
             <div class="small_box">
                 <div class="small_img">
                     <a href="{{ route('xero_commerce::product.show', ['slug' => $products['center_up']->getSlug()]) }}">
-                        <img src="{{ $products['center_up']->getThumbnailSrc() }}">
+                        <img src="{{ $products['center_up']->getThumbnailSrc() }}" style="width: 367px; height: 238px;">
                         <div class="inner_box">
                             <strong>{{ $products['center_up']->name }}</strong>
                             <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
@@ -25,7 +25,7 @@
                 </div>
                 <div class="small_img">
                     <a href="{{ route('xero_commerce::product.show', ['slug' => $products['center_down']->getSlug()]) }}">
-                        <img src="{{ $products['center_down']->getThumbnailSrc() }}">
+                        <img src="{{ $products['center_down']->getThumbnailSrc() }}" style="width: 367px; height: 238px;">
                         <div class="inner_box">
                             <strong>{{ $products['center_down']->name }}</strong>
                             <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
@@ -35,7 +35,7 @@
             </div>
             <div class="large_img">
                 <a href="{{ route('xero_commerce::product.show', ['slug' => $products['right']->getSlug()]) }}">
-                    <img src="{{ $products['right']->getThumbnailSrc() }}">
+                    <img src="{{ $products['right']->getThumbnailSrc() }}" style="width: 367px; height: 490px;">
                     <div class="inner_box">
                         <strong>{{ $products['right']->name }}</strong>
                         <p>KNIT 이벤트 내용 KNIT 이벤트 내용 KNIT 이벤 트 내용 KNIT 이벤트 내용 KNIT 이벤트 내용 KNI T 이벤트</p>
