72997ab6add4bdc51ea186b74d2e77c1ed7e1693
sirwoongke <fzwoongbin@naver.com>
2018-11-07 17:21:33 +0900

    상품 정렬 리팩토링

--- src/Components/Widget/ProductListWidget/Skins/Common/views/widget.blade.php
+++ src/Components/Widget/ProductListWidget/Skins/Common/views/widget.blade.php
@@ -1,10 +1,12 @@
+<?php
+use Xpressengine\Plugins\XeroCommerce\Handlers\ProductHandler;
+?>
 {{ XeFrontend::css('plugins/xero_commerce/src/Components/Widget/ProductListWidget/Skins/Common/assets/style.css')->load() }}
 
 <section class="xe-shop list">
     <div class="container" style="padding-left:0; padding-right:0">
         <div class="search-results">
-
-            @if(count($products)===0)
+            @if ($products->total() == 0)
                 <p class="search-results-text">검색된 상품이 존재하지 않습니다.</p>
             @else
                 <p class="search-results-text"><span class="search-results-text-num">{{ $products->total() }}</span>개의 상품이
@@ -14,32 +16,27 @@
         <form action="{{url()->current()}}" method="get">
             <div class="range-box">
                 <div class="research-box">
-                    <input type="text" name="product_name" class="xe-form-control" placeholder>
+                    <input type="text" name="product_name" class="xe-form-control" placeholder value="{{( request()->product_name)? : ''}}">
                     <button type="submit">
                         <i class="xi-search"></i><span class="xe-sr-only">검색</span>
                     </button>
                 </div>
 
-
                 <div class="xe-dropdown">
-                    <button class="xe-btn" type="button" data-toggle="xe-dropdown">Low Price</button>
+                    <button class="xe-btn" type="button" >{{ request()->sort_type? ProductHandler::getSortAble()[request()->sort_type] : '정렬 옵션' }}</button>
                     <ul class="xe-dropdown-menu">
-                        <li><a href="#">text</a></li>
-                        <li><a href="#">text</a></li>
-                        <li><a href="#">text</a></li>
-                        <li><a href="#">text</a></li>
+                        <li><a href="#">정렬 옵션</a></li>
+                        @foreach (ProductHandler::getSortAble() as $key => $sort)
+                            <li><a href="#" value="{{ $key }}">{{ $sort }}</a></li>
+                        @endforeach
                     </ul>
+                    <input type="hidden" name="sort_type">
                 </div>
-                <script>
-                    $('.xe-dropdown .xe-btn, .xe-dropdown-menu a').on('click', function () {
-                        $('.xe-dropdown').toggleClass('open');
-                    });
-                </script>
             </div>
         </form>
 
         <ul class="default-list">
-            @foreach ($products as $key=>$product)
+            @foreach ($products as $key => $product)
                 <li>
                     <div class="default-list-img">
                         <a href="{{ route('xero_commerce::product.show', ['slug' => $product->getSlug()]) }}"><img
@@ -47,16 +44,19 @@
                         <h4 class="xe-sr-only">sns 공유</h4>
                         <ul class="default-list-sns">
                             <!-- [D] a 클릭시 내부에 xi-heart-o 클래스 명을 xi-heart 로 변경 부탁드립니다. -->
-                            <li><a href="#"><i class="xi-heart"></i><span class="xe-sr-only">좋아요</span></a></li>
+                            <li><a href="#" onclick="event.preventDefault();toggleHeart('{{$product->id}}')"><i id="heart{{$product->id}}" class="{{($product->userWish())? 'xi-heart' : 'xi-heart-o'}}"></i><span class="xe-sr-only">좋아요</span></a></li>
                             <li><a href="#"><i class="xi-facebook"></i><span class="xe-sr-only">페이스북 공유</span></a></li>
                             <li><a href="#"><i class="xi-instagram"></i><span class="xe-sr-only">인스타그램 공유</span></a>
                             </li>
                         </ul>
                     </div>
                     <div class="default-list-text">
-                        <a href="{{ route('xero_commerce::product.show', ['slug' => $product->getSlug()]) }}"><h3
-                                    class="default-list-text-title"><span class="xe-shop-tag black">new</span><span
-                                        class="xe-shop-tag">best</span> {{$product->name}}</h3>
+                        <a href="{{ route('xero_commerce::product.show', ['slug' => $product->getSlug()]) }}">
+                            <h3 class="default-list-text-title">
+                                @foreach($product->labels as $label)
+                                    <span class="xe-shop-tag" @if($label->background_color && $label->text_color)style="background: {{$label->background_color}}; color:{{$label->text_color}}" @endif>{{$label->name}}</span>
+                                @endforeach
+                                {{$product->name}}</h3>
                             <p class="default-list-text-price">
                                 <span class="xe-sr-only">할인 전</span>
                                 <span class="through">{{ number_format($product->original_price)}}원</span>
@@ -75,3 +75,35 @@
         {{ $products->render() }}
     </div>
 </section>
+
+<script>
+    function toggleHeart(id)
+    {
+        $.ajax({
+            url: '{{route('xero_commerce::product.wish.toggle',['product'=>''])}}/'+id
+        }).done(()=>{
+            toggleClass('#heart'+id,'xi-heart-o')
+            toggleClass('#heart'+id,'xi-heart')
+        })
+    }
+    function toggleClass(target, className)
+    {
+        if ($(target).hasClass(className)){
+            $(target).removeClass(className)
+        } else {
+            $(target).addClass(className)
+        }
+    }
+
+    $('.xe-dropdown .xe-btn, .xe-dropdown-menu a').on('click', function () {
+        toggleClass('.xe-dropdown','open')
+    });
+
+    $('.xe-dropdown-menu a').on('click', function (e) {
+        e.preventDefault();
+
+        $("input[name=sort_type]").val($(this).attr('value'));
+
+        $("form").submit();
+    });
+</script>
