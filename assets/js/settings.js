d4743a089f2711ae118377feeea557d35c546a86
bnu <bnufactory@gmail.com>
2018-11-28 17:51:35 +0900

    validation 실패 시 포커스, 라벨 붙을 때 panel-heading에도 붙이기

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,14 +1,23 @@
-window.jQuery(function ($) {
-  var XE = window.XE
-  XE.app('Form').then(function (appForm) {
-    // 상품 등록 폼
-    var productForm = appForm.get($('#save'))
-    console.debug('productForm', productForm)
+window.XE.app('Form').then(function (appForm) {
+  // 상품 등록 폼
+  var productForm = appForm.get($('#save'))
+  console.debug('productForm', productForm)
 
-    productForm.$$on('xe.validation.faield', function (eventName, data) {
-      var $field = data.field
+  // field에 포커스
+  productForm.$$on('xe.validation.faield', function (eventName, data) {
+    var $field = data.field
+    var collapse = $field.closest('.collapse')
+    if (collapse.length && !collapse.is(':visible')) {
+      collapse.collapse('show')
+    }
+    $field.focus()
+  })
 
-      // $field.focus()
-    })
+  productForm.$$on('xe.valitation.required-label', function (eventName, { field, label }) {
+    console.debug('xe.valitation.required-label', label)
+    var headingTitle = label.closest('.panel').find('.panel-title')
+    if (headingTitle.length) {
+      headingTitle.addClass('xe-form__label--requried')
+    }
   })
 })
