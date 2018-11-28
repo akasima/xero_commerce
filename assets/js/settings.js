d59b5b724c7993113bae79eb173dfaf35f9aa925
bnu <bnufactory@gmail.com>
2018-11-28 17:54:02 +0900

    console 제거

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,7 +1,6 @@
 window.XE.app('Form').then(function (appForm) {
   // 상품 등록 폼
   var productForm = appForm.get($('#save'))
-  console.debug('productForm', productForm)
 
   // field에 포커스
   productForm.$$on('xe.validation.faield', function (eventName, data) {
@@ -14,7 +13,6 @@ window.XE.app('Form').then(function (appForm) {
   })
 
   productForm.$$on('xe.valitation.required-label', function (eventName, { field, label }) {
-    console.debug('xe.valitation.required-label', label)
     var headingTitle = label.closest('.panel').find('.panel-title')
     if (headingTitle.length) {
       headingTitle.addClass('xe-form__label--requried')
