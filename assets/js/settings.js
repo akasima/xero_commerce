931f435112c1b7c859ee8050371d7a417165b26f
bnu <bnufactory@gmail.com>
2018-11-29 11:36:00 +0900

    상품 등록페이지가 아닐 때 스크립트 오류 수정

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,21 +1,23 @@
 window.XE.app('Form').then(function (appForm) {
   // 상품 등록 폼
-  var productForm = appForm.get($('#save'))
+  if ($('#save').length) {
+    var productForm = appForm.get($('#save'))
 
-  // field에 포커스
-  productForm.$$on('xe.validation.faield', function (eventName, data) {
-    var $field = data.field
-    var collapse = $field.closest('.collapse')
-    if (collapse.length && !collapse.is(':visible')) {
-      collapse.collapse('show')
-    }
-    $field.focus()
-  })
+    // field에 포커스
+    productForm.$$on('xe.validation.faield', function (eventName, data) {
+      var $field = data.field
+      var collapse = $field.closest('.collapse')
+      if (collapse.length && !collapse.is(':visible')) {
+        collapse.collapse('show')
+      }
+      $field.focus()
+    })
 
-  productForm.$$on('xe.valitation.required-label', function (eventName, { field, label }) {
-    var headingTitle = label.closest('.panel').find('.panel-title')
-    if (headingTitle.length) {
-      headingTitle.addClass('xe-form__label--requried')
-    }
-  })
+    productForm.$$on('xe.valitation.required-label', function (eventName, { field, label }) {
+      var headingTitle = label.closest('.panel').find('.panel-title')
+      if (headingTitle.length) {
+        headingTitle.addClass('xe-form__label--requried')
+      }
+    })
+  }
 })
