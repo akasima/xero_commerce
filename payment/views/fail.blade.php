8b13faf5e2b469f5d41c551018117c3dcc8484e7
Hero <kimyu711@xpressengine.com>
2018-12-04 12:58:59 +0900

    polyfill 제공

--- payment/views/fail.blade.php
+++ payment/views/fail.blade.php
@@ -2,6 +2,24 @@
 <body onload="func()">
 </body>
 <script>
+    (function () {
+        if (
+            typeof window.CustomEvent === "function" ||
+            // In Safari, typeof CustomEvent == 'object' but it otherwise works fine
+            this.CustomEvent.toString().indexOf('CustomEventConstructor')>-1
+        ) { return; }
+
+        function CustomEvent ( event, params ) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent( 'CustomEvent' );
+            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+            return evt;
+        }
+
+        CustomEvent.prototype = window.Event.prototype;
+
+        window.CustomEvent = CustomEvent;
+    })();
     function func (){
         setTimeout(function(){
             var ev = new CustomEvent('fail',{
