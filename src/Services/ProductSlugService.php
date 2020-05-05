11ab7b802563399bee1bd2df37ac7516ae116f33
Darron Park <darron1217@gmail.com>
2020-05-05 19:43:09 +0900

    PHP 7.4 버전 호환

--- src/Services/ProductSlugService.php
+++ src/Services/ProductSlugService.php
@@ -125,7 +125,7 @@ class ProductSlugService
         if ($len <= 0) {
             return false;
         }
-        $h = ord($ch{0});
+        $h = ord($ch[0]);
         if ($h <= 0x7F) {
             return $h;
         }
@@ -133,13 +133,13 @@ class ProductSlugService
             return false;
         }
         if ($h <= 0xDF && $len>1) {
-            return ($h & 0x1F) <<  6 | (ord($ch{1}) & 0x3F);
+            return ($h & 0x1F) <<  6 | (ord($ch[1]) & 0x3F);
         }
         if ($h <= 0xEF && $len>2) {
-            return ($h & 0x0F) << 12 | (ord($ch{1}) & 0x3F) << 6 | (ord($ch{2}) & 0x3F);
+            return ($h & 0x0F) << 12 | (ord($ch[1]) & 0x3F) << 6 | (ord($ch[2]) & 0x3F);
         }
         if ($h <= 0xF4 && $len>3) {
-            return ($h & 0x0F) << 18 | (ord($ch{1}) & 0x3F) << 12 | (ord($ch{2}) & 0x3F) << 6 | (ord($ch{3}) & 0x3F);
+            return ($h & 0x0F) << 18 | (ord($ch[1]) & 0x3F) << 12 | (ord($ch[2]) & 0x3F) << 6 | (ord($ch[3]) & 0x3F);
         }
 
         return false;
